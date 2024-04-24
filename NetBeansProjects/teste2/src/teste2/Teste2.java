66e704b49d834c6d6da3a59df841134516cbe566
Maicon <MaiconLucio@outlook.com>
2024-04-24 09:30:17 -0500

    all java'archives

---
+++ NetBeansProjects/teste2/src/teste2/Teste2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package teste2;
+
+/**
+ *
+ * @author Maicon
+ */
+public class Teste2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        * The code below will print the words Hello World
+        * to the screen, and it is amazing
+        */
+         System.out.println("Hello World"); //this is a comment
+           
+}
+                
+                
+    }
+ 
